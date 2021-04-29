a7c15761aa9ea6dda22654ef7ec68816abec3d2e
kiathomsen1996 <82096714+kiathomsen1996@users.noreply.github.com>
2021-04-29 15:43:50 +0200

    added adaptor to project

--- app/src/main/java/dk/au/mad21spring/group20/knittybuddy/MenuActivity.java
+++ app/src/main/java/dk/au/mad21spring/group20/knittybuddy/MenuActivity.java
@@ -8,17 +8,28 @@ import android.view.View;
 import android.widget.Button;
 
 import dk.au.mad21spring.group20.knittybuddy.feed.FeedActivity;
+import dk.au.mad21spring.group20.knittybuddy.project.ProjectEditActivity;
+import dk.au.mad21spring.group20.knittybuddy.project.ProjectListActivity;
 
 public class MenuActivity extends AppCompatActivity {
 
+    //widgets
     private Button feedBtn;
+    private Button projectsBtn;
+
+    //attributes
     public static final int REQUEST_CODE_FEED = 101;
+    public static final int REQUEST_CODE_PROJECTS = 201;
+
+    //life cycle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
         feedBtn = findViewById(R.id.feedBtn);
+        projectsBtn = findViewById(R.id.btn_menu_project);
+
         feedBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -26,12 +37,23 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
+        projectsBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                goToProjectList();
+            }
+        });
     }
 
+    //methods
     private void goToFeed(){
         Intent intent = new Intent(this, FeedActivity.class);
         startActivityForResult(intent,REQUEST_CODE_FEED );
+    }
 
+    private void goToProjectList(){
+        Intent intent = new Intent(this, ProjectListActivity.class);
+        startActivityForResult(intent,REQUEST_CODE_PROJECTS );
     }
 
 }
--- app/src/main/java/dk/au/mad21spring/group20/knittybuddy/project/ProjectAdapter.java
+++ app/src/main/java/dk/au/mad21spring/group20/knittybuddy/project/ProjectAdapter.java
@@ -1,4 +1,67 @@
 package dk.au.mad21spring.group20.knittybuddy.project;
 
-public class ProjectAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import dk.au.mad21spring.group20.knittybuddy.R;
+
+//this class is inspired by lecture 3 "ListAndGrid" demo code
+//this class handles the scrolling of projects in the project list
+public class ProjectAdapter extends RecyclerView.Adapter<ProjectAdapter.ProjectViewHolder> {
+
+    //internal interface
+    public interface IProjectItemClickedListener{
+        void onProjectClicked(int index);
+    }
+
+    //attributes
+    private List<Project> projectList;
+    private IProjectItemClickedListener listener;
+
+    //constructor
+    public ProjectAdapter(IProjectItemClickedListener listener) {this.listener = listener;}
+
+    //methods
+    public void updateProjectList(List<Project> list){
+        projectList = list;
+        notifyDataSetChanged();
+    }
+
+    //internal class
+    public class ProjectViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public ProjectViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+
+    //methods from internal class "ProjectViewHolder"
+    @NonNull
+    @Override
+    public ProjectViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProjectViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
 }

a563925667678a0b1582ef59747e35a2598008a7
Solguden <55831381+Solguden@users.noreply.github.com>
2021-05-03 21:30:55 +0200

    namespacefix

---
+++ app/src/main/java/dk/au/mad21spring/group20/knittybuddy/PDFActivity.java
@@ -0,0 +1,14 @@
+package dk.au.mad21spring.group20.knittybuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PDFActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p_d_f);
+    }
+}

37f8cd8a6e67982217904aa41a826ec214d11a7e
kiathomsen1996 <82096714+kiathomsen1996@users.noreply.github.com>
2021-05-03 10:27:37 +0200

    app logo, projectList, project adaptor and projectDetails

--- app/src/main/java/dk/au/mad21spring/group20/knittybuddy/project/ProjectAdapter.java
+++ app/src/main/java/dk/au/mad21spring/group20/knittybuddy/project/ProjectAdapter.java
@@ -3,10 +3,13 @@ package dk.au.mad21spring.group20.knittybuddy.project;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import dk.au.mad21spring.group20.knittybuddy.R;
@@ -21,7 +24,7 @@ public class ProjectAdapter extends RecyclerView.Adapter<ProjectAdapter.ProjectV
     }
 
     //attributes
-    private List<Project> projectList;
+    private List<Project> projectList = new ArrayList<Project>();
     private IProjectItemClickedListener listener;
 
     //constructor
@@ -36,32 +39,51 @@ public class ProjectAdapter extends RecyclerView.Adapter<ProjectAdapter.ProjectV
     //internal class
     public class ProjectViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
-        public ProjectViewHolder(@NonNull View itemView) {
+        //widget
+        Button projectName;
+
+        ProjectAdapter.IProjectItemClickedListener listener;
+
+        public ProjectViewHolder(@NonNull View itemView, ProjectAdapter.IProjectItemClickedListener projectItemClickedListener) {
             super(itemView);
+
+            listener = projectItemClickedListener;
+
+            //instantiation of widget
+            projectName = itemView.findViewById(R.id.btn_project);
+
+            itemView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
-
+            listener.onProjectClicked(getAdapterPosition());
         }
     }
-
 
     //methods from internal class "ProjectViewHolder"
     @NonNull
     @Override
     public ProjectViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v;
+        v = LayoutInflater.from(parent.getContext()).inflate(R.layout.project_list_item,parent,false);
+
+        ProjectViewHolder vh = new ProjectViewHolder(v, listener);
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ProjectViewHolder holder, int position) {
-
+        holder.projectName.setText(projectList.get(position).getName());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if (projectList != null)
+        {
+            return projectList.size();
+        }
+        else return 0;
     }
 
 }
--- app/src/main/java/dk/au/mad21spring/group20/knittybuddy/project/ProjectDetailsActivity.java
+++ app/src/main/java/dk/au/mad21spring/group20/knittybuddy/project/ProjectDetailsActivity.java
@@ -2,15 +2,73 @@ package dk.au.mad21spring.group20.knittybuddy.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import dk.au.mad21spring.group20.knittybuddy.R;
 
 public class ProjectDetailsActivity extends AppCompatActivity {
 
+    //widgets
+    TextView txtProjectName;
+    TextView txtProjectDetails;
+    Button btnGoBack;
+
+    //attributes
+    private int projectId;
+    //test
+    private List<Project> projectList;
+
+    //life cycle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_detail);
+
+        //instantiation of widgets
+        txtProjectName = findViewById(R.id.txt_projectDetailHeader);
+        txtProjectDetails = findViewById(R.id.txt_projectDetailText);
+        btnGoBack = findViewById(R.id.btn_projectDetail_back);
+        projectId = 0; //default value
+
+        //get data from ProjectListActivity, who started this activity
+        Intent data = getIntent();
+        projectId = data.getIntExtra("id",0); //TO DO: hardcode name and default
+
+        //button listener
+        btnGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        //test
+        projectList = new ArrayList<Project>();
+        Project testProject1 = new Project(1, "Sommerkjole", "Fin luftig kjole", "111", "pdfLink");
+        Project testProject2 = new Project(2, "Vinter trøje", "Dejlig varm sweather", "222", "pdfLink");
+        projectList.add(testProject1);
+        projectList.add(testProject2);
+        updateUI(projectId);
+    }
+
+    //methods
+    private void updateUI(int id){
+        if  (id != 0){
+            txtProjectName.setText(projectList.get(id).getName());
+            txtProjectDetails.setText(projectList.get(id).getDescription());
+        }
+        else{
+            Project prjDefault = new Project(0, "default", "default", "default", "default");
+            txtProjectName.setText(prjDefault.getName());
+            txtProjectDetails.setText(prjDefault.getDescription());
+        }
+
     }
 }

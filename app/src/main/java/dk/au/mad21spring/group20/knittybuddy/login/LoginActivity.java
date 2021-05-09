29365ac7c444a89ddb25d9698b688c930054eb9f
Solguden <55831381+Solguden@users.noreply.github.com>
2021-05-09 13:55:55 +0200

    login

--- app/src/main/java/dk/au/mad21spring/group20/knittybuddy/login/LoginActivity.java
+++ app/src/main/java/dk/au/mad21spring/group20/knittybuddy/login/LoginActivity.java
@@ -7,12 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import dk.au.mad21spring.group20.knittybuddy.MenuActivity;
 import dk.au.mad21spring.group20.knittybuddy.PDFActivity;
 import dk.au.mad21spring.group20.knittybuddy.R;
 
 public class LoginActivity extends AppCompatActivity {
     Button loginBtn;
     Button registerBtn;
+    public static final int REQUEST_CODE_MENU = 444;
+    public static final int REQUEST_CODE_REGISTER = 555;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,13 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                gotoMenu();
+            }
+        });
+
 
         registerBtn = findViewById(R.id.registerRegisterBtn);
         registerBtn.setOnClickListener(new View.OnClickListener(){
@@ -30,8 +40,13 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    private void gotoMenu() {
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivityForResult(intent,REQUEST_CODE_MENU );
+    }
+
     private void gotoRegister() {
         Intent intent = new Intent(this, RegisterActivity.class);
-        startActivityForResult(intent,123 );
+        startActivityForResult(intent,REQUEST_CODE_REGISTER );
     }
 }
